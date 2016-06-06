369c4c1304c03b194b43dd844bb86e8f4410dff1
redaphid <iam@aaronherres.com>
2016-06-05 19:00:57 -0700

    clojure. it hurts the brain

---
+++ power-of-thor.clj
@@ -0,0 +1,23 @@
+(ns Player
+  (:gen-class))
+
+(defn -main [& args]
+  (let [lightX (read) lightY (read) initialTX (read) initialTY (read)
+        vectorX (if (< lightX initialTX) (repeat (- initialTX lightX) "W" ) (repeat (- lightX initialTX) "E" ))
+        vectorY (if (< lightY initialTY) (repeat (- initialTY lightY) "N" ) (repeat (- lightY initialTY) "S" ))
+        ]
+
+
+  (defn turn [vectorX vectorY] (
+    (let [remainingTurns (read) x (first vectorX) y (first vectorY)]
+      (if y (print y))
+      (if x (print x))
+      (println)
+      (binding [*out* *err*]
+        (println vectorX vectorY))
+    )
+    (turn (rest vectorX) (rest vectorY))
+  ))
+
+  (turn vectorX vectorY))
+)
